80a18ab9e1bb7771607001a335e149c1c8773f72
Nazar Tytskyi <nazartitskiy@gmail.com>
2024-12-05 08:18:00 +0200

    Added UI Component - Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+import styles from './Button.module.scss';
+
+interface IButtonProps {
+  children: ReactNode;
+  onClick?: () => void;
+}
+
+export function Button({ children, onClick }: IButtonProps) {
+  return <button onClick={onClick} className={styles.button} type="button">
+    {children}
+  </button>
+}
